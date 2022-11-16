d6c6e51d7aed8b7d2af052cd212187bc3e392424
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2022-11-15 22:36:08 -0300

    listando professores

---
+++ app/src/main/java/br/gabriel/hachathon4periodo/Config.java
@@ -0,0 +1,6 @@
+package br.gabriel.hachathon4periodo;
+
+public class Config {
+    public static String urlApi = "http://10.1.1.9:3333/professores";
+    public static String linkImagem = "http://127.0.0.1:3333/static/";
+}
