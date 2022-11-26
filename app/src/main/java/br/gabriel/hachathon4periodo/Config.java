581492512aa669b1dd37d72ca2c45b9117fbd9e9
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2022-11-25 22:59:41 -0300

    bg

--- app/src/main/java/br/gabriel/hachathon4periodo/Config.java
+++ app/src/main/java/br/gabriel/hachathon4periodo/Config.java
@@ -1,6 +1,6 @@
 package br.gabriel.hachathon4periodo;
 
 public class Config {
-    public static String urlApi = "http://10.1.1.9:3333/professores";
-    public static String linkImagem = "http://10.1.1.9:3333/static/";
+    public static String urlApi = "http://10.1.1.12:3333/professores";
+    public static String linkImagem = "http://10.1.1.12:3333/static/";
 }
